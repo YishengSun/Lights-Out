4972bcff3e28341253fb63f618d452718a0a210f
YishengSun <yisheng4@illinois.edu>
2020-01-16 16:32:30 -0600

    Updates

---
+++ precache-manifest.69bec189c00956e75a8a8677f088a13e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3825e61c471be6e706b2b39a59c865",
+    "url": "/Lights-Out/index.html"
+  },
+  {
+    "revision": "ba7f54f25157e95375f2",
+    "url": "/Lights-Out/static/css/main.cb3829b4.chunk.css"
+  },
+  {
+    "revision": "94bcc578d2eacee4dd6f",
+    "url": "/Lights-Out/static/js/2.9af8ae1c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Lights-Out/static/js/2.9af8ae1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba7f54f25157e95375f2",
+    "url": "/Lights-Out/static/js/main.78bb5895.chunk.js"
+  },
+  {
+    "revision": "b7d7aca8665f3f90415b",
+    "url": "/Lights-Out/static/js/runtime-main.fe4ac1d6.js"
+  }
+]);
